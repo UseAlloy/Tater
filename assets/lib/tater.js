51869dbf2f9983f88a360685e04f9939ebc6695e
Scott Clark <scott@alloy.co>
2016-12-15 15:58:44 -0500

    Update tater.js

--- assets/lib/tater.js
+++ assets/lib/tater.js
@@ -65,7 +65,7 @@
 /******/ 	}
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "7cb53d62eb4c5cd6a764"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "2c1d387aa927445ba60c"; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentParents = []; // eslint-disable-line no-unused-vars
 /******/ 	
