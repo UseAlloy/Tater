1a53c2f97233082366baeb0a4b58d727e73c97df
jcsicsek <jcsicsek@gmail.com>
2016-12-15 14:40:54 -0500

    route to get all data from any given table

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const Express = require('express');
 const SequelizeAuto = require('sequelize-auto');
+const Sequelize = require('sequelize');
+const fs = require("fs");
+const path= require("path");
 
 const app = Express();
 const auto = new SequelizeAuto('alloy', 'postgres', '', {
@@ -14,14 +17,44 @@ const auto = new SequelizeAuto('alloy', 'postgres', '', {
       schema: 'alloy',
     }
 });
+const sequelize = new Sequelize('alloy', 'postgres', '', {
+  host: 'localhost',
+  schema: 'alloy',
+  dialect: 'postgres',
+  port: '5432',
+  schema: 'alloy',
+  dialectOptions: {
+    schema: 'alloy',
+  }
+});
 
-app.get('/tables', (req, res) => {
-  auto.run((err) => {
-    if (err) throw err;
+auto.run((err) => {
+  const models = {};
+
+  fs.readdirSync(__dirname + "/models")
+    .filter(function(file) {
+      return (file.indexOf(".") !== 0) && (file !== "index.js");
+    })
+    .forEach(function(file) {
+      models[file.slice(0, -3)] = sequelize.import(path.join(__dirname + "/models", file));
+    });
+
+  app.get('/tables', (req, res) => {
     res.send(auto.tables);
   });
-});
 
-app.listen(3000, function () {
-  console.log('App listening on port 3000')
-});
+  app.get('/tables/:table', (req, res) => {
+    models[req.params.table].schema('alloy').findAll({})
+      .then((data) => {
+        res.send(data);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.error("Goddam error");
+      });
+  });
+
+  app.listen(3000, function () {
+    console.log('App listening on port 3000')
+  });
+})
